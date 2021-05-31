2512434fe85999a567f18e355ba6eacc7c3a117e
Karen Mirakyan <karenmirakyan@gmail.com>
2021-05-31 13:30:23 +0400

    Initial Commit

---
+++ Secret Language/Secret_LanguageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Secret_LanguageApp.swift
+//  Secret Language
+//
+//  Created by Karen Mirakyan on 31.05.21.
+//
+
+import SwiftUI
+
+@main
+struct Secret_LanguageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
