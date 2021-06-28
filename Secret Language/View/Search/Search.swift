85dcf277c20bfc4404d928e93642bb2b86993ccd
Karen Mirakyan <karenmirakyan@gmail.com>
2021-06-28 17:56:32 +0400

    get interests and select from them the ideal for parameter

--- Secret Language/View/Search/Search.swift
+++ Secret Language/View/Search/Search.swift
@@ -42,6 +42,33 @@ struct Search: View {
                                 .font(.custom("Gilroy-Regular", size: 14))
                         }
                         
+                        HStack {
+                            ForEach( searchVM.ideals, id: \.id ) { ideal in
+                                Spacer()
+                                
+                                VStack( spacing: 4) {
+                                    
+                                    Button {
+                                        withAnimation {
+                                            searchVM.ideal = ideal.id
+                                        }
+                                    } label: {
+                                        Text( ideal.name )
+                                            .font(.custom("times", size: 16))
+                                            .foregroundColor(searchVM.ideal == ideal.id ? .accentColor : .gray)
+                                            .padding(.vertical, 8)
+                                            .padding(.horizontal, 18)
+                                            .background(RoundedRectangle(cornerRadius: 4)
+                                                            .strokeBorder(searchVM.ideal == ideal.id ? AppColors.accentColor : Color.clear, lineWidth: 1)
+                                                            .background(searchVM.ideal == ideal.id ? .black : AppColors.boxColor)
+                                            )
+                                    }
+                                    
+                                }
+                                Spacer()
+                            }
+                        }
+                        
                     }.padding()
                     
                     let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 2)
@@ -62,8 +89,15 @@ struct Search: View {
                     }
                 }.padding(.top, 1)
                 
+                CustomAlert(isPresented: $searchVM.showAlert, alertMessage: searchVM.alertMessage, alignment: .center)
+                    .offset(y: searchVM.showAlert ? 0 : UIScreen.main.bounds.size.height)
+                    .animation(.interpolatingSpring(mass: 0.3, stiffness: 100.0, damping: 50, initialVelocity: 0))
+                
             }.navigationBarTitle("")
             .navigationBarTitleView(SearchNavBar(title: "Community" ), displayMode: .inline)
+            .onAppear(perform: {
+                searchVM.getIdealCategories()
+            })
             .onTapGesture {
                 UIApplication.shared.endEditing()
             }
--- Secret Language/ViewModels/SearchViewModel.swift
+++ Secret Language/ViewModels/SearchViewModel.swift
@@ -14,17 +14,22 @@ class SearchViewModel: ObservableObject {
     @AppStorage( "token" ) private var token: String = ""
     @Published var search: String = ""
     @Published var searchResults = [UserPreviewModel(id: 1, name: "Adhraaa Al Azimi", image: "https://sln-storage.s3.us-east-2.amazonaws.com/user/default.png", ideal: "Business"), UserPreviewModel(id: 2, name: "John Smith", image: "https://sln-storage.s3.us-east-2.amazonaws.com/user/default.png", ideal: "Business"), UserPreviewModel(id: 3, name: "John Smith", image: "https://sln-storage.s3.us-east-2.amazonaws.com/user/default.png", ideal: "Business")]
-    @Published var ideal: Int = 0
+    @Published var ideal: Int = 1
     
     @Published var loading: Bool = false
     @Published var showAlert: Bool = false
     @Published var alertMessage: String = ""
     
+    @Published var ideals = [ConnectionTypeModel]()
+    
     private var cancellableSet: Set<AnyCancellable> = []
     var dataManager: SearchServiceProtocol
+    var authDataManager: AuthServiceProtocol
     
-    init(dataManager: SearchServiceProtocol = SearchService.shared) {
+    init(dataManager: SearchServiceProtocol = SearchService.shared,
+         authDataManager: AuthServiceProtocol = AuthService.shared) {
         self.dataManager = dataManager
+        self.authDataManager = authDataManager
         
         $search
             .removeDuplicates()
@@ -57,6 +62,20 @@ class SearchViewModel: ObservableObject {
                 self.loading = false
                 if response.error == nil {
                     self.searchResults = response.value!
+                }
+            }.store(in: &cancellableSet)
+    }
+    
+    func getIdealCategories() {
+        loading = true
+        authDataManager.fetchConnectionTypes()
+            .sink { response in
+                self.loading = false
+                if response.error != nil {
+                    self.alertMessage = response.error!.backendError == nil ? response.error!.initialError.localizedDescription : response.error!.backendError!.message
+                    self.showAlert.toggle()
+                } else {
+                    self.ideals = response.value!
                 }
             }.store(in: &cancellableSet)
     }
