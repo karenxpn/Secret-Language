88249ae90f187d6f644e63bd4ac9ef814f2d309e
KALIMI <karenmirakyan@gmail.com>
2021-07-29 01:35:50 +0400

    removed unnecessary lines

--- Secret Language/View/Chats/ChatRoom.swift
+++ Secret Language/View/Chats/ChatRoom.swift
@@ -14,7 +14,7 @@ struct ChatRoom: View {
     
     @StateObject var roomVM = MessageRoomViewModel()
     @State private var navigateToUser: Bool = false
-
+    
     var body: some View {
         ZStack(alignment: .bottom) {
             Background()
@@ -26,7 +26,7 @@ struct ChatRoom: View {
             NavigationLink(destination: VisitedProfile(userID: user.id, userName: user.name), isActive: $navigateToUser) {
                 EmptyView()
             }.hidden()
-
+            
             VStack(spacing: 0) {
                 MessagesList(roomID: roomID, username: user.name)
                     .environmentObject(roomVM)
@@ -41,25 +41,18 @@ struct ChatRoom: View {
                 
                 MessageBar().environmentObject(roomVM)
             }
-
+            
         }.navigationBarTitle("")
         .navigationBarTitleView(ChatRoomNavBar(navigate: $navigateToUser, user: user))
         .onAppear {
             NotificationCenter.default.post(name: Notification.Name("hideTabBar"), object: nil)
             
-            // check if the roomID is 0 -> create new room
-            // else get messages
+            roomVM.roomID = roomID
+            roomVM.getChatRoomMessagesWithPusher()
+            roomVM.getTypingStatus()
             
-            if roomID == 0 {
-                
-            } else {
-                roomVM.roomID = roomID
-                roomVM.getChatRoomMessagesWithPusher()
-                roomVM.getTypingStatus()
-                
-                if roomVM.messages.isEmpty {
-                    roomVM.getChatRoomMessages( lastMessageID: 0)
-                }
+            if roomVM.messages.isEmpty {
+                roomVM.getChatRoomMessages( lastMessageID: 0)
             }
         }.onDisappear {
             NotificationCenter.default.post(name: Notification.Name("showTabBar"), object: nil)
@@ -73,18 +66,18 @@ struct ChatRoom: View {
             } else if roomVM.activeSheet == .camera {
                 CameraView()
                     .environmentObject(roomVM)
-            }else {
-               EmptyView()
+            } else {
+                EmptyView()
             }
-
+            
         }.actionSheet(item: $roomVM.action) { value in
             
             if value == .message {
                 return ActionSheet(title: Text( NSLocalizedString("actions", comment: "") ), message: nil, buttons: [.default(Text( "Copy" ), action: {
                     UIPasteboard.general.string = roomVM.actionItem?.content[0].message
                 }), .destructive(Text( "Delete" ), action: {
-//                  delete item here
-//                  print("delete item \(roomVM.actionItem?.id)")
+                    //                  delete item here
+                    //                  print("delete item \(roomVM.actionItem?.id)")
                 }), .cancel()])
             } else {
                 return ActionSheet(title: Text( NSLocalizedString("selectSource", comment: "") ), message: nil, buttons: [.default(Text( "Gallery" ), action: {
