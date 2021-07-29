2880e63cfe8447d224ae6c2106254355fc98d10b
KALIMI <karenmirakyan@gmail.com>
2021-07-29 23:07:23 +0400

    navigate from requests to visited profile added

--- Secret Language/View/Profile/PendingListCell.swift
+++ Secret Language/View/Profile/PendingListCell.swift
@@ -15,48 +15,50 @@ struct PendingListCell: View {
     
     var body: some View {
         
-        HStack( spacing: 20) {
-            WebImage(url: URL(string: pendingRequest.image))
-                .placeholder {
-                    ProgressView()
-                }.resizable()
-                .scaledToFill()
-                .frame(width: 55, height: 55)
-                .clipShape(Circle())
-            
-            VStack( alignment: .leading) {
-                Text( pendingRequest.name )
-                    .foregroundColor(.white)
-                    .font(.custom("times", size: 18))
+        NavigationLink(destination: VisitedProfile(userID: pendingRequest.id, userName: pendingRequest.name)) {
+            HStack( spacing: 20) {
+                WebImage(url: URL(string: pendingRequest.image))
+                    .placeholder {
+                        ProgressView()
+                    }.resizable()
+                    .scaledToFill()
+                    .frame(width: 55, height: 55)
+                    .clipShape(Circle())
                 
-                HStack( spacing: 0) {
-                    Text( "\(NSLocalizedString("idealFor", comment: ""))")
-                        .foregroundColor(.gray)
-                        .font(.custom("Gilroy-Regular", size: 15))
+                VStack( alignment: .leading) {
+                    Text( pendingRequest.name )
+                        .foregroundColor(.white)
+                        .font(.custom("times", size: 18))
                     
-                    Text( pendingRequest.ideal)
-                        .foregroundColor(.accentColor)
-                        .font(.custom("Gilroy-Regular", size: 15))
-                        .lineLimit(1)
+                    HStack( spacing: 0) {
+                        Text( "\(NSLocalizedString("idealFor", comment: ""))")
+                            .foregroundColor(.gray)
+                            .font(.custom("Gilroy-Regular", size: 15))
+                        
+                        Text( pendingRequest.ideal)
+                            .foregroundColor(.accentColor)
+                            .font(.custom("Gilroy-Regular", size: 15))
+                            .lineLimit(1)
+                    }
                 }
-            }
-            
-            Spacer()
-            
-            Button(action: {
-                profileVM.withdrawFriendRequest(userID: pendingRequest.id)
-            }, label: {
-                Text(NSLocalizedString("withdraw", comment: ""))
-                    .foregroundColor( .accentColor )
-                    .font(.custom("Gilroy-Regular", size: 14))
-                    .padding(.vertical, 8)
-                    .padding(.horizontal, 18)
-                    .background(RoundedRectangle(cornerRadius: 4)
-                                    .strokeBorder(AppColors.accentColor, lineWidth: 1.5)
-                                    .background(AppColors.dataFilterGendersBg)
-                )
-            })
-        }.padding()
+                
+                Spacer()
+                
+                Button(action: {
+                    profileVM.withdrawFriendRequest(userID: pendingRequest.id)
+                }, label: {
+                    Text(NSLocalizedString("withdraw", comment: ""))
+                        .foregroundColor( .accentColor )
+                        .font(.custom("Gilroy-Regular", size: 14))
+                        .padding(.vertical, 8)
+                        .padding(.horizontal, 18)
+                        .background(RoundedRectangle(cornerRadius: 4)
+                                        .strokeBorder(AppColors.accentColor, lineWidth: 1.5)
+                                        .background(AppColors.dataFilterGendersBg)
+                    )
+                })
+            }.padding()
+        }
     }
 }
 
