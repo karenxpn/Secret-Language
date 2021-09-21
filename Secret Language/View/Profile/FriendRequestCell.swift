c39613ba852e26d7f32d27844238324c92b65f0d
KALIMI <karenmirakyan@gmail.com>
2021-09-21 04:04:19 +0400

    friend requests list modified for both 15 and lower

--- Secret Language/View/Profile/FriendRequestCell.swift
+++ Secret Language/View/Profile/FriendRequestCell.swift
@@ -16,56 +16,108 @@ struct FriendRequestCell: View {
     
     var body: some View {
         
-        Button {
-            active.toggle()
-        } label: {
-            HStack( spacing: 20) {
-                WebImage(url: URL(string: request.image))
-                    .placeholder {
-                        ProgressView()
-                    }.resizable()
-                    .scaledToFill()
-                    .frame(width: 55, height: 55)
-                    .clipShape(Circle())
-                
-                VStack( alignment: .leading) {
-                    Text( request.name )
-                        .foregroundColor(.white)
-                        .font(.custom("times", size: 18))
+        if #available(iOS 15.0, *) {
+            Button {
+                active.toggle()
+            } label: {
+                HStack( spacing: 20) {
+                    WebImage(url: URL(string: request.image))
+                        .placeholder {
+                            ProgressView()
+                        }.resizable()
+                        .scaledToFill()
+                        .frame(width: 55, height: 55)
+                        .clipShape(Circle())
                     
-                    HStack( spacing: 0) {
-                        Text( "\(NSLocalizedString("idealFor", comment: ""))")
-                            .foregroundColor(.gray)
-                            .font(.custom("Gilroy-Regular", size: 15))
+                    VStack( alignment: .leading) {
+                        Text( request.name )
+                            .foregroundColor(.white)
+                            .font(.custom("times", size: 18))
                         
-                        Text( request.ideal)
-                            .foregroundColor(AppColors.accentColor)
-                            .font(.custom("Gilroy-Regular", size: 15))
-                            .lineLimit(1)
+                        HStack( spacing: 0) {
+                            Text( "\(NSLocalizedString("idealFor", comment: ""))")
+                                .foregroundColor(.gray)
+                                .font(.custom("Gilroy-Regular", size: 15))
+                            
+                            Text( request.ideal)
+                                .foregroundColor(AppColors.accentColor)
+                                .font(.custom("Gilroy-Regular", size: 15))
+                                .lineLimit(1)
+                        }
                     }
-                }
-                
-                Spacer()
-                
-                Button(action: {
-                    profileVM.acceptFriendRequest(userID: request.id)
-                }, label: {
-                    Text(NSLocalizedString("accept", comment: ""))
-                        .foregroundColor( AppColors.accentColor )
-                        .font(.custom("Gilroy-Regular", size: 14))
-                        .padding(.vertical, 8)
-                        .padding(.horizontal, 18)
-                        .background(RoundedRectangle(cornerRadius: 4)
-                                        .strokeBorder(AppColors.accentColor, lineWidth: 1.5)
-                                        .background(AppColors.dataFilterGendersBg)
-                        )
-                })
-            }.padding(.bottom)
-        }.background(
-            NavigationLink(destination: VisitedProfile(userID: request.id, userName: request.name), isActive: $active) {
-                EmptyView()
-            }.hidden()
-        )
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        profileVM.acceptFriendRequest(userID: request.id)
+                    }, label: {
+                        Text(NSLocalizedString("accept", comment: ""))
+                            .foregroundColor( AppColors.accentColor )
+                            .font(.custom("Gilroy-Regular", size: 14))
+                            .padding(.vertical, 8)
+                            .padding(.horizontal, 18)
+                            .background(RoundedRectangle(cornerRadius: 4)
+                                            .strokeBorder(AppColors.accentColor, lineWidth: 1.5)
+                                            .background(AppColors.dataFilterGendersBg)
+                            )
+                    })
+                }.padding(.bottom)
+            }.background(
+                NavigationLink(destination: VisitedProfile(userID: request.id, userName: request.name), isActive: $active) {
+                    EmptyView()
+                }.hidden()
+            )
+        } else {
+            Button {
+              } label: {
+                  HStack( spacing: 20) {
+                      WebImage(url: URL(string: request.image))
+                          .placeholder {
+                              ProgressView()
+                          }.resizable()
+                          .scaledToFill()
+                          .frame(width: 55, height: 55)
+                          .clipShape(Circle())
+                      
+                      VStack( alignment: .leading) {
+                          Text( request.name )
+                              .foregroundColor(.white)
+                              .font(.custom("times", size: 18))
+                          
+                          HStack( spacing: 0) {
+                              Text( "\(NSLocalizedString("idealFor", comment: ""))")
+                                  .foregroundColor(.gray)
+                                  .font(.custom("Gilroy-Regular", size: 15))
+                              
+                              Text( request.ideal)
+                                  .foregroundColor(.accentColor)
+                                  .font(.custom("Gilroy-Regular", size: 15))
+                                  .lineLimit(1)
+                          }
+                      }
+                      
+                      Spacer()
+                      
+                      Button(action: {
+                          profileVM.acceptFriendRequest(userID: request.id)
+                      }, label: {
+                          Text(NSLocalizedString("accept", comment: ""))
+                              .foregroundColor( .accentColor )
+                              .font(.custom("Gilroy-Regular", size: 14))
+                              .padding(.vertical, 8)
+                              .padding(.horizontal, 18)
+                              .background(RoundedRectangle(cornerRadius: 4)
+                                              .strokeBorder(AppColors.accentColor, lineWidth: 1.5)
+                                              .background(AppColors.dataFilterGendersBg)
+                              )
+                      })
+                  }.padding()
+              }.background(
+                  NavigationLink(destination: VisitedProfile(userID: request.id, userName: request.name)) {
+                      EmptyView()
+                  }.hidden()
+              )
+        }
     }
 }
 
