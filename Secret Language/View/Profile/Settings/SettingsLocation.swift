74ac2aca36fd03eab8ab7a22863df82a02a17a87
KALIMI <karenmirakyan@gmail.com>
2021-09-30 02:14:05 +0400

    Visited Profile action button moved under the image

---
+++ Secret Language/View/Profile/Settings/SettingsLocation.swift
@@ -0,0 +1,23 @@
+//
+//  SettingsLocation.swift
+//  Secret Language
+//
+//  Created by Karen Mirakyan on 30.09.21.
+//
+
+import SwiftUI
+
+struct SettingsLocation: View {
+    
+    
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsLocation_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsLocation()
+    }
+}
