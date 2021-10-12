54775ff77ba80e5ea2a8ddd10baa954210bc5201
KALIMI <karenmirakyan@gmail.com>
2021-10-12 16:19:06 +0400

    MonthReportInnerView added

--- Secret Language/View/Reports/BirthdayReport/MonthReport.swift
+++ Secret Language/View/Reports/BirthdayReport/MonthReport.swift
@@ -17,34 +17,7 @@ struct MonthReport: View {
         
         ZStack {
             Background()
-            
-            ScrollView( showsIndicators: false ) {
-                VStack( alignment: .leading) {
-                    VStack {
-                        
-                        Text( report.span1 )
-                            .foregroundColor(.black)
-                            .font(.custom("times", size: 16))
-
-                        ImageHelper(image: report.image, contentMode: .fit, progressViewTintColor: .gray)
-                            .frame(width: .greedy, height: 150)
-                            .padding()
-                        
-                        Text( report.name )
-                            .foregroundColor(.black)
-                            .font(.custom("times", size: 22))
-                            .fontWeight(.heavy)
-                            .multilineTextAlignment(.center)
-                    }.padding()
-                    .background(.white)
-                    .padding(.bottom)
-                    
-                    ReportSection(title: NSLocalizedString("theirMode", comment: ""), content: report.mode, orientation: 2)
-                    ReportSection(title: NSLocalizedString("motto", comment: ""), content: report.motto, orientation: 2)
-                    ReportSection(title: NSLocalizedString("personality", comment: ""), content: report.report)
-                }.padding(.bottom, UIScreen.main.bounds.size.height * 0.15)
-            }.padding(.top, 1)
-
+            MonthReportInnerView(report: report)
         }.navigationBarTitle( "" )
         .navigationBarTitleView(SearchNavBar(title: report.name), displayMode: .inline)
         .navigationBarItems(trailing:
