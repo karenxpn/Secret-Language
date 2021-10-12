0a12886deb3ddeee97b94c2d2a1cbb5279a40e82
KALIMI <karenmirakyan@gmail.com>
2021-10-12 16:20:13 +0400

    SeasonReportInnerView added

---
+++ Secret Language/View/Reports/BirthdayReport/SeasonReportInnerView.swift
@@ -0,0 +1,45 @@
+//
+//  SeasonReportInnerView.swift
+//  Secret Language
+//
+//  Created by Karen Mirakyan on 12.10.21.
+//
+
+import SwiftUI
+
+struct SeasonReportInnerView: View {
+    let report: SeasonReportModel
+
+    var body: some View {
+        ScrollView( showsIndicators: false ) {
+            VStack( alignment: .leading) {
+                VStack {
+                    
+                    Text( report.span1 )
+                        .foregroundColor(.black)
+                        .font(.custom("times", size: 18))
+                        .fontWeight(.semibold)
+
+                    ImageHelper(image: report.image, contentMode: .fit, progressViewTintColor: .gray)
+                        .frame(width: .greedy, height: 150)
+                        .padding()
+                    
+                    Text( report.name )
+                        .foregroundColor(.black)
+                        .font(.custom("times", size: 22))
+                        .fontWeight(.heavy)
+                        .multilineTextAlignment(.center)
+                    
+                }.padding()
+                .background(.white)
+                .padding(.bottom)
+                
+                ReportSection(title: NSLocalizedString("description", comment: ""), content: report.description, orientation: 2)
+                
+                ReportSection(title: NSLocalizedString("faculty", comment: ""), content: report.faculty, orientation: 2)
+                
+                ReportSection(title: NSLocalizedString("personality", comment: ""), content: report.report)
+            }.padding(.bottom, UIScreen.main.bounds.size.height * 0.15)
+        }.padding(.top, 1)
+    }
+}
