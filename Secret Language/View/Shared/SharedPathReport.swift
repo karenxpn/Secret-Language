8a47d3c06dbf3a6d2ff6df08383aed78922eebda
KALIMI <karenmirakyan@gmail.com>
2021-10-13 00:01:16 +0400

    shared path, way reports added

---
+++ Secret Language/View/Shared/SharedPathReport.swift
@@ -0,0 +1,53 @@
+//
+//  SharedPathReport.swift
+//  Secret Language
+//
+//  Created by Karen Mirakyan on 12.10.21.
+//
+
+import SwiftUI
+
+struct SharedPathReport: View {
+    @Environment(\.presentationMode) var presentationMode
+    @ObservedObject var reportVM = ReportViewModel()
+    let reportID: Int
+    
+    var body: some View {
+        
+        NavigationView {
+            ZStack {
+                Background()
+                
+                if reportVM.loading {
+                    ProgressView()
+                } else if reportVM.pathReport != nil {
+                    PathReportInnerView(report: reportVM.pathReport!)
+                }
+                
+                CustomAlert(isPresented: $reportVM.showAlert, alertMessage: reportVM.alertMessage, alignment: .center)
+                    .offset(y: reportVM.showAlert ? 0 : UIScreen.main.bounds.size.height)
+                    .animation(.interpolatingSpring(mass: 0.3, stiffness: 100.0, damping: 50, initialVelocity: 0))
+                
+            }.navigationBarTitle( "" )
+            .navigationBarTitleView(SearchNavBar(title: "Path Report"), displayMode: .inline)
+            .navigationBarItems(trailing: Button(action: {
+                presentationMode.wrappedValue.dismiss()
+            }, label: {
+                Image("close")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 16, height: 16)
+                    .padding([.leading, .top, .bottom])
+                
+            })).onAppear {
+                reportVM.getSharedPathReport(reportID: reportID)
+            }
+        }.navigationViewStyle(StackNavigationViewStyle())
+    }
+}
+
+struct SharedPathReport_Previews: PreviewProvider {
+    static var previews: some View {
+        SharedPathReport(reportID: 1)
+    }
+}
---
+++ Secret Language/View/Shared/SharedWayReport.swift
@@ -0,0 +1,47 @@
+//
+//  SharedWayReport.swift
+//  Secret Language
+//
+//  Created by Karen Mirakyan on 12.10.21.
+//
+
+import SwiftUI
+
+struct SharedWayReport: View {
+    @Environment(\.presentationMode) var presentationMode
+    @ObservedObject var reportVM = ReportViewModel()
+    let reportID: Int
+    
+    var body: some View {
+        
+        NavigationView {
+            ZStack {
+                Background()
+                
+                if reportVM.loading {
+                    ProgressView()
+                } else if reportVM.wayReport != nil {
+                    WayReportInnerView(report: reportVM.wayReport!)
+                }
+                
+                CustomAlert(isPresented: $reportVM.showAlert, alertMessage: reportVM.alertMessage, alignment: .center)
+                    .offset(y: reportVM.showAlert ? 0 : UIScreen.main.bounds.size.height)
+                    .animation(.interpolatingSpring(mass: 0.3, stiffness: 100.0, damping: 50, initialVelocity: 0))
+                
+            }.navigationBarTitle( "" )
+            .navigationBarTitleView(SearchNavBar(title: "Way Report"), displayMode: .inline)
+            .navigationBarItems(trailing: Button(action: {
+                presentationMode.wrappedValue.dismiss()
+            }, label: {
+                Image("close")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 16, height: 16)
+                    .padding([.leading, .top, .bottom])
+                
+            })).onAppear {
+                reportVM.getSharedWayReport(reportID: reportID)
+            }
+        }.navigationViewStyle(StackNavigationViewStyle())
+    }
+}
