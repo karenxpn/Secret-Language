af7dd6d86541c9386d687b4cdd19ebd717f6c062
KALIMI <karenmirakyan@gmail.com>
2021-10-14 00:21:02 +0400

    unauthorized error code added

--- Secret Language/ViewModels/AlertViewModel.swift
+++ Secret Language/ViewModels/AlertViewModel.swift
@@ -6,10 +6,24 @@
 //
 
 import Foundation
+import SwiftUI
+
 class AlertViewModel {
+    
+    @AppStorage("token") private var token: String = ""
+    @AppStorage("username") private var username: String = ""
+    @AppStorage( "userID" ) private var userID: Int = 0
+    
     func makeAlert(with error: NetworkError, message: inout String, alert: inout Bool ) {
-        message = error.backendError == nil ? error.initialError.localizedDescription : error.backendError!.message
-        alert.toggle()
+        
+        if error.initialError.responseCode == 401 {
+            self.token = ""
+            self.username = ""
+            self.userID = 0
+        } else {
+            message = error.backendError == nil ? error.initialError.localizedDescription : error.backendError!.message
+            alert.toggle()
+        }
     }
     
     func makeSuccessAlert( with response: GlobalResponse, showAlert: inout Bool, alertMessage: inout String ) {
